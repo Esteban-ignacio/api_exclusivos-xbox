f712f2940965ec8eb027781c4ead3817117dde4b
Esteban <es.alamos@duocuc.cl>
2024-06-02 19:05:50 -0400

    juegos exclusivos nintendo

---
+++ juegos.js
@@ -0,0 +1,63 @@
+
+
+const juegos = [
+    {
+        "id": "1",
+        "name": "Crash Bandicoot 4 Its About Time",
+        "img": "https://juegosdigitaleschile.com/files/images/productos/1649193517-crash-bandicoot-4-its-about-time-xbox-series-xs-0.jpg",
+        "consola": "Xbox Series x|s",
+        "precio": "9.000",
+        "cantidad": 24,
+        "descripcion": "Es una secuela digna de los juegos originales de la serie, ofreciendo una experiencia de plataformas divertida y desafiante que encantará tanto a los fanáticos de toda la vida como a los nuevos jugadores. Con su variedad de personajes jugables, mundos coloridos y mecánicas de juego innovadoras, el juego captura la esencia de la serie mientras introduce nuevas ideas y emocionantes desafíos para los jugadores."
+    },   
+    {
+        "id": "2",
+        "name": "Jump Force",
+        "img": "https://juegosdigitaleschile.com/files/images/productos/1651790900-jump-force-xbox-series-xs-0.jpg",
+        "consola": "Xbox Series x|s",
+        "precio": "15.000",
+        "cantidad": 28,
+        "descripcion": "Es un juego que cumple el sueño de muchos fanáticos de los mangas y anime al permitirles controlar a sus personajes favoritos en batallas épicas. Aunque no sin sus críticas, el juego ofrece una experiencia emocionante y visualmente impresionante para los aficionados a las series de la revista Shonen Jump."
+    },
+    {
+        "id": "3",
+        "name": "Marvels Avengers",
+        "img": "https://juegosdigitaleschile.com/files/images/productos/1648593580-marvels-avengers-xbox-one-0.jpg",
+        "consola": "Xbox One",
+        "precio": "16.000",
+        "cantidad": 34,
+        "descripcion": "Es un juego ambicioso que busca ofrecer una experiencia emocionante para los fanáticos de los superhéroes de Marvel. Con una historia original, una variedad de personajes jugables y modos de juego cooperativos, el juego ofrece una inmersión en el universo de los Vengadores. "
+    },
+    {
+        "id": "4",
+        "name": "Call Of Duty Vanguard",
+        "img": "https://juegosdigitaleschile.com/files/images/productos/1648332725-call-of-duty-vanguard-xbox-series-xs-0.jpg",
+        "consola": "Xbox Series x|s",
+        "precio": "50.000",
+        "cantidad": 19,
+        "descripcion": "Combina una narrativa inmersiva de la Segunda Guerra Mundial con modos multijugador robustos y el siempre popular modo Zombies."
+    },
+    {
+        "id": "5",
+        "name": "Need For Speed Payback",
+        "img": "https://juegosdigitaleschile.com/files/images/productos/1651695907-need-for-speed-payback-xbox-series-xs-0.jpg",
+        "consola": "Xbox Series x|s",
+        "precio": "14.000",
+        "cantidad": 44,
+        "descripcion": "Combina carreras de alta velocidad, una narrativa de venganza al estilo de una película de acción, y una extensa personalización de vehículos en un mundo abierto variado y visualmente impresionante."
+    },
+    {
+        "id": "6",
+        "name": "Far Cry 4",
+        "img": "https://juegosdigitaleschile.com/files/images/productos/1645829068-far-cry-4-xbox-one.jpg",
+        "consola": "Xbox One",
+        "precio": "28.000",
+        "cantidad": 40,
+        "descripcion": "Es un juego de acción y aventura en primera persona que combina un mundo abierto detallado con una narrativa intrigante y una jugabilidad flexible, proporcionando una experiencia envolvente en el conflictivo y hermoso mundo de Kyrat."
+    },
+
+];
+
+module.exports = {
+    juegos
+} 
